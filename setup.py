96436605e05a54512eb92c5a4d088f268b635cf9
simon-schaefer <simon.k.schaefer@gmail.com>
2024-05-22 19:47:11 +0200

    Version: 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clockwise',
-    version='1.0.1',
+    version='1.0.2',
     description='A simple timing library for Python.',
     author='Simon Schaefer',
     author_email='simon.k.schaefer@gmail.com',
